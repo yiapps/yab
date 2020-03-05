cf7733ea1063793936fa96287acdf8ee7a6b95e8
Alex Gleason <alex@alexgleason.me>
2020-03-05 16:43:28 -0600

    Use BLOCKED_EMAILS for EmailPermutationValidator

--- app/validators/email_permutation_validator.rb
+++ app/validators/email_permutation_validator.rb
@@ -3,13 +3,18 @@
 class EmailPermutationValidator < ActiveModel::Validator
   def validate(user)
     @email = user.email
+    @blocked_emails = ENV['BLOCKED_EMAILS'].split(',')
     user.errors.add(:email, I18n.t('users.invalid_email')) if blocked_email?
   end
 
   private
 
   def blocked_email?
-    # TODO
-    # @email
+    @blocked_emails.include? stripped_dots_email
+  end
+
+  def stripped_dots_email
+    username, domain = @email.split('@')
+    "#{username.gsub('.', '')}@#{domain}"
   end
 end
